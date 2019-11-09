55d8fd04bc69e111963e0a1b225ddff9c06c9700
shuheiokuda0704 <shuheiokuda0704@gmail.com>
2019-11-09 21:11:54 +0900

    Implement == method in SoftDrink class

--- lib/soft_drink.rb
+++ lib/soft_drink.rb
@@ -1,4 +1,13 @@
 class SoftDrink
+  attr_reader :name, :price
+
   def initialize(name:, price:)
+    @name = name
+    @price = price
+  end
+
+  def ==(other)
+    self.name == other.name &&
+        self.price == other.price
   end
 end
