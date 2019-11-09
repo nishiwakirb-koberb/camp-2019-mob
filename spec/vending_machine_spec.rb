34976aa6f6d7b7b1ba571a2ed0f1b8e62ade9bdc
rururu16 <ynk618172@gmail.com>
2019-11-09 16:13:06 +0900

    仮実装

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,4 +1,9 @@
 require 'spec_helper'
 
-RSpec.describe '' do
+RSpec.describe 'Vending machine' do
+  example 'step 0' do
+    # 10円玉、50円玉、100円玉、500円玉、1000円札を１つずつ投入できる。
+    vending_machine = VendingMachine.new
+    vending_machine.insert_money(10)
+  end
 end
