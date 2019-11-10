77a168742919e0248c43517fbff06e325f7c22a7
Junichi Ito <jit@sonicgarden.jp>
2019-11-11 08:35:06 +0900

    Add spec for SoftDrink#eql?

--- spec/soft_drink_spec.rb
+++ spec/soft_drink_spec.rb
@@ -16,4 +16,19 @@ RSpec.describe SoftDrink do
       expect(coke).to_not eq other
     end
   end
+
+  describe '#eql?' do
+    example 'ユニーク判定の条件が正しい' do
+      drink_list = [
+        *Array.new(5) { SoftDrink.new(name: 'コーラ', price: 120) },
+        *Array.new(1) { SoftDrink.new(name: 'コーラ', price: 180) },
+        *Array.new(3) { SoftDrink.new(name: 'ファンタ', price: 140) },
+      ]
+      expect(drink_list.uniq).to contain_exactly(
+                                   SoftDrink.new(name: 'コーラ', price: 120),
+                                   SoftDrink.new(name: 'コーラ', price: 180),
+                                   SoftDrink.new(name: 'ファンタ', price: 140)
+                                 )
+    end
+  end
 end
